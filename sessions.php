8c1718950cd41eaf7205c102b3b65de6218af805
AngelaGu <angela.gu1224@gmail.com>
2023-02-17 04:59:58 -0800

    changes

--- sessions.php
+++ sessions.php
@@ -1,3 +1,11 @@
-$file = "sessions.txt";
-$test = $_POST["sessionid"];
-file_put_contents($file, $text . "\n");
+<html>
+<body>
+    Session ID <?php echo $_POST["sessionid"]; ?>
+
+    <?php $file = "sessions.txt";
+        $test = $_POST["sessionid"];
+        file_put_contents($file, $text . "\n"); ?>
+
+</body>
+
+</html>
