e3680eba15bff736ded0f09f419f811118adb6f9
AngelaGu <angela.gu1224@gmail.com>
2023-03-16 05:20:40 -0700

    hw2

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 // references:
-// [41] https://requestbin.com/ --> request bin 
-// [43] https://www.w3schools.com/jsref/met_win_setinterval.asp --> setInterval() usage 
-// [46] https://developer.mozilla.org/en-US/docs/Web/API/Document/forms --> for making forms and accessing their values
-// [32] https://www.w3schools.com/jsref/prop_node_textcontent.asp --> textContent usage
-// [47] https://developer.mozilla.org/en-US/docs/Web/API/Document/documentElement --> to get entire document element
-// [48] https://www.w3schools.com/jsref/event_onerror.asp --> onerror event usage 
+// [42] https://requestbin.com/ --> request bin 
+// [44] https://www.w3schools.com/jsref/met_win_setinterval.asp --> setInterval() usage 
+// [48] https://developer.mozilla.org/en-US/docs/Web/API/Document/forms --> for making forms and accessing their values
+// [33] https://www.w3schools.com/jsref/prop_node_textcontent.asp --> textContent usage
+// [49] https://developer.mozilla.org/en-US/docs/Web/API/Document/documentElement --> to get entire document element
+// [50] https://www.w3schools.com/jsref/event_onerror.asp --> onerror event usage 
+
 function changeTitle(){
     // in 100 ms, change the page title to be the XSS payload 
     window.setInterval(function(){
